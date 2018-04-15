d5dc742dfc37c360418fea40192ff8c0e1e4b04a
程嵇邢滑 <xingdl2007@gmail.com>
2018-04-15 18:34:05 +0800

    log: multithreaded async logging

--- tests/logging_test.cc
+++ tests/logging_test.cc
@@ -73,3 +73,58 @@ TEST(AsyncLogging, Basic2) {
   ::sleep(1);
   async.StopWorker();
 }
+
+TEST(AsyncLogging, MultiThread) {
+  setLogLevel(LogLevel::TRACE);
+  AsyncLogging async("async-basic2");
+
+  Logger::destination([&](Slice const &slice) {
+    async.Append(slice);
+  });
+
+  async.StartWorker();
+
+  Slice slice("123456789 hello world ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ");
+
+  int count = 5000000;
+
+  std::promise<void> go, t1, t2;
+  std::shared_future<void> ready = go.get_future();
+
+  auto func1 = [&, ready]() {
+    t1.set_value();
+    ready.wait();
+    for (int i = 0; i < count / 2; ++i) {
+      LOG_INFO << slice << i + 1;
+    }
+  };
+  auto func2 = [&, ready]() {
+    t2.set_value();
+    ready.wait();
+    for (int i = 0; i < count / 2; ++i) {
+      LOG_INFO << slice << i + 1;
+    }
+  };
+
+  std::thread thread1(func1);
+  std::thread thread2(func2);
+
+  // ready?
+  t1.get_future().wait();
+  t2.get_future().wait();
+
+  // go!
+  auto start = std::chrono::system_clock::now();
+  go.set_value();
+  thread1.join();
+  thread2.join();
+
+  std::chrono::nanoseconds elapse = std::chrono::system_clock::now() - start;
+  auto seconds = std::chrono::duration_cast<std::chrono::seconds>(elapse).count();
+  auto data = slice.size() * count * 1.0 / 1024 / 1024;
+  std::cerr << seconds << " s, " << data / seconds << " MiB/s\n";
+
+  // make sure worker thread is waiting
+  ::sleep(1);
+  async.StopWorker();
+}
