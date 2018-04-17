6e94fc0f5d64117ea3039184fc9da0088d5e2fa9
程嵇邢滑 <xingdl2007@gmail.com>
2018-04-17 12:06:07 +0800

    verify fstream flush does not call fsync

--- examples/echo/main.cc
+++ examples/echo/main.cc
@@ -2,6 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <fstream>
+
 int main() {
+  // flush does not call fsync, just like fflush()
+  std::fstream file("test.log");
+  file << "from file stream";
+  file.flush();
+  file.close();
+
+  // fsync
+  char buffer[32] = "hello test.log\n";
+  int fd = ::open("test.log", O_WRONLY, "ae");
+  ::write(fd, buffer, sizeof buffer);
+  ::fsync(fd);
+  ::close(fd);
   return 0;
 }
